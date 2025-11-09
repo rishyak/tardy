402b808b2b624d83c278eaf9fd5499132f69bb37
rishyak <hello@rishyak.com>
2025-11-09 01:07:46 -0500

    Add configurable time format

--- src/tardyclock/cli.py
+++ src/tardyclock/cli.py
@@ -1,12 +1,28 @@
 import typer
 from .core import random_time
 
+DEFAULT_TIME_FORMAT = "%A %B %m, %Y, %H:%M:%S"
+DEFAULT_TIME_FORMAT_12_HOUR = "%A %B %m, %Y, %I:%M:%S"
+
 tardy = typer.Typer()
 
 
 @tardy.command()
-def tell(hr24: bool = True, showtimezone: bool = False):
-  print(random_time(hr24=hr24, showtimezone=showtimezone))
+def tell(
+  hr24: bool = True,
+  showtimezone: bool = False,
+  time_format: str = DEFAULT_TIME_FORMAT,
+):
+  if not hr24 and time_format == DEFAULT_TIME_FORMAT:
+    time_format = DEFAULT_TIME_FORMAT_12_HOUR
+
+  print(
+    random_time(
+      hr24=hr24,
+      showtimezone=showtimezone,
+      time_format=time_format,
+    )
+  )
 
 
 if __name__ == "__main__":
